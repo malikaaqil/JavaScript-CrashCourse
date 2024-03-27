85eb9f382ce4a6243855f0c17c8a99ec839f9af1
Malik Aaqil <malikaaqil2000@gmail.com>
2024-03-27 05:03:45 +0500

    class-01

---
+++ class-01/app.js
@@ -0,0 +1,33 @@
+// alert("Hello World");
+
+let greet = "Hello";
+// alert(greet);
+
+
+// legal variable names:
+
+let name;
+let myName;
+let myName1;
+let _myName;
+let my_name;
+let $myName;
+
+// illegal varible names:
+
+// let 1num;
+// let my num;
+// let my-num
+// let @mynum
+
+
+// data types:
+
+// 1.string:
+// exapmle: "", "Sam", "1234", "@", "true";
+
+// 2.number:
+// example: 23, -23, 2.23;
+
+// 3.boolean:
+// exapmle: true & false;
