879e967f5d4d8ec27834706c015a44eec0bb0785
Malik Aaqil <malikaaqil2000@gmail.com>
2024-04-07 02:35:02 +0500

    class-08

--- class-08/app.js
+++ class-08/app.js
@@ -1,1 +1,37 @@
-alert()
+// let date = new Date();
+// let monthsNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+// console.log(date.getMonth());
+// console.log(monthsNames[date.getMonth()]);
+
+// console.log(date.getDay());
+// console.log(date.getHours());
+
+
+// date.setDate(10)
+// console.log(date);
+
+
+// let hoursElm = document.getElementById("hours");
+// let minutesElm = document.getElementById("minutes");
+// let secondsElm = document.getElementById("seconds");
+// let amPmElm = document.getElementById("amPm");
+
+// setInterval(function () {
+
+//     let date = new Date();
+//     let hours = date.getHours();
+
+//     if (hours > 11) {
+//         amPmElm.innerHTML = "pm";
+
+//         if (hours > 12) {
+//             hours = hours - 12;
+//         }
+//     }
+
+//     hoursElm.innerHTML = hours;
+//     minutesElm.innerHTML = date.getMinutes();
+//     secondsElm.innerHTML = date.getSeconds();
+
+// }, 1000)
